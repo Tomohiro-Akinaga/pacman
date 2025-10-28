02ac3c62c83803fdfe43244a5ee4905ce89b5b04
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-10-28 12:57:46 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,11 +3,16 @@ if (!canvas) throw new Error("Canvas not found");
 
 const ctx = canvas.getContext("2d");
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
 class Boundary {
   position: { x: number; y: number };
   width: number;
   height: number;
 
+  static width = 40;
+  static height = 40;
   constructor({ position }) {
     this.position = position;
     this.width = 40;
@@ -24,6 +29,7 @@ class Boundary {
 const map = [
   ["-", "-", "-", "-", "-", "-"],
   ["-", " ", " ", " ", " ", "-"],
+  ["-", " ", "-", "-", " ", "-"],
   ["-", " ", " ", " ", " ", "-"],
   ["-", "-", "-", "-", "-", "-"],
 ];
@@ -34,7 +40,7 @@ map.forEach((raw, i) => {
   raw.forEach((symbol, j) => {
     switch (symbol) {
       case "-":
-        boundaries.push(new Boundary({ position: { x: 40 * j, y: 40 * i } }));
+        boundaries.push(new Boundary({ position: { x: Boundary.width * j, y: Boundary.height * i } }));
         break;
     }
   });
