4d71f8d3eea8a8dfecf14aae1d3d61bf79d791ac
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-10-31 15:57:17 +0800

    feat: serveを追加

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-var canvas = document.querySelector("canvas");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const canvas = document.querySelector("canvas");
 if (!canvas)
     throw new Error("Canvas not found");
-var ctx = canvas.getContext("2d");
-var Boundary = /** @class */ (function () {
-    function Boundary(_a) {
-        var position = _a.position;
+const ctx = canvas.getContext("2d");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+class Boundary {
+    position;
+    width;
+    height;
+    static width = 40;
+    static height = 40;
+    constructor({ position }) {
         this.position = position;
         this.width = 40;
         this.height = 40;
     }
-    Boundary.prototype.draw = function () {
+    draw() {
         if (!ctx)
             throw new Error("2D context not found");
         ctx.fillStyle = "blue";
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    };
-    return Boundary;
-}());
-var map = [
+    }
+}
+const map = [
     ["-", "-", "-", "-", "-", "-"],
     ["-", " ", " ", " ", " ", "-"],
+    ["-", " ", "-", "-", " ", "-"],
     ["-", " ", " ", " ", " ", "-"],
     ["-", "-", "-", "-", "-", "-"],
 ];
-var boundaries = [];
-map.forEach(function (raw, i) {
-    raw.forEach(function (symbol, j) {
+const boundaries = [];
+map.forEach((raw, i) => {
+    raw.forEach((symbol, j) => {
         switch (symbol) {
             case "-":
-                boundaries.push(new Boundary({ position: { x: 40 * j, y: 40 * i } }));
+                boundaries.push(new Boundary({ position: { x: Boundary.width * j, y: Boundary.height * i } }));
                 break;
         }
     });
 });
-boundaries.forEach(function (boundary) { return boundary.draw(); });
+boundaries.forEach((boundary) => boundary.draw());
+//# sourceMappingURL=index.js.map
